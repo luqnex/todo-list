79fd47a9db39cb626bb34fad2493e69df5593297
Lucas Costa <lucas.costa4002@gmail.com>
2021-08-06 21:07:39 -0300

    Projeto 1.0

---
+++ src/components/TodoList.jsx
@@ -0,0 +1,45 @@
+import React, { useState } from 'react'
+import './TodoList.css'
+
+export function TodoList() {
+    
+    //Cria o estado para salvar o input
+    const [task, setTask] = useState('')
+    
+    //Capturar o input do usuario
+    function handleInputTask(event) {
+        const inputTask = event.target.value
+        
+        const isInputTask = inputTask.value != undefined ? inputTask : 'Digite um valor'
+        
+        setTask(isInputTask)
+    }
+    
+    const [itemsList, setItemsList] = useState([])
+    
+    //Funcao que adiciona um item na lista
+    function addItem(event) {
+        event.preventDefault()
+        setItemsList([...itemsList, task])
+        
+        setTask('')
+    }
+    
+    return (
+        <div className="todo-wrapper">
+            <h1>Todo List</h1>
+            <div className="list">
+                <form >
+                    <input type="text" placeholder="Adicione uma tarefa" onChange={handleInputTask} value={task} />
+                    <button type="submit" onClick={addItem}>Adicionar</button>
+                </form>
+        
+                <ul className="todo-list" >
+                    {itemsList.map(item => 
+                        <li>{item}</li>
+                    )}
+                </ul>
+            </div>
+        </div>
+    )
+}
